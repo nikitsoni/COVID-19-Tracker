b8b584c1789ce9670b5c979fb39e090ceddf777b
nikitsoni <47641941+nikitsoni@users.noreply.github.com>
2020-05-13 00:15:40 +0530

    Updates

---
+++ precache-manifest.931b128464da6269ff2e77ac8bafa1c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "209aa39e10d8c664a03f4bb7247f631c",
+    "url": "/COVID-19-Tracker/index.html"
+  },
+  {
+    "revision": "ad99ca6123a5a2292f5a",
+    "url": "/COVID-19-Tracker/static/css/main.613d8ae3.chunk.css"
+  },
+  {
+    "revision": "5de59414f4f40d98289e",
+    "url": "/COVID-19-Tracker/static/js/2.75ea25fd.chunk.js"
+  },
+  {
+    "revision": "b4cabae32341920cfb4131dbdebda2c2",
+    "url": "/COVID-19-Tracker/static/js/2.75ea25fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad99ca6123a5a2292f5a",
+    "url": "/COVID-19-Tracker/static/js/main.0bbe3c37.chunk.js"
+  },
+  {
+    "revision": "9514b4d9d1d40672788b",
+    "url": "/COVID-19-Tracker/static/js/runtime-main.47e3ab38.js"
+  }
+]);
